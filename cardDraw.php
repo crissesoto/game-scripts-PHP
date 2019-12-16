f87859ffc7e17e125c7db68bb82d9f1dc806482f
crissesoto <soto.crisse@hotmail.com>
2019-12-16 10:01:47 +0100

    Calculating the odds of drawing specific card

--- cardDraw.php
+++ cardDraw.php
@@ -22,10 +22,13 @@ foreach($faces as $face){
 };
 
 shuffle($deck);
+
 $card = array_shift($deck);
 
 //echo $card["face"] ." of ". $card["suit"];
 
+
+// Drawing two hands of five cards each
 $hand = [1 => array(), 2 => array()];
 
 for($i=0; $i <= 5; $i++){
@@ -33,9 +36,39 @@ for($i=0; $i <= 5; $i++){
   $hand[2][] = implode(" of ", array_shift($deck));
 }
 
-print_r($hand[1]);
-echo "<br>";
-print_r($hand[2]);
+//print_r($hand[1]);
+//echo "<br>";
+//print_r($hand[2]);
+
+
+// Looking for a specific card = $draw
+$draw = array('face' => 'Ace', 'suit' => 'Clubs');
+
+
+// Calculating the odds of drawing specific card
+function calculate_match($draw, $deck){
+  // we can look in the deck to see how many cards are left
+  $remaining = count($deck);
+  $match = 0;
+
+  foreach($deck as $card){
+    if(
+      ($draw["face"] == $card["face"] && $draw["suit"] == $card['suit']) 
+      ||
+      ($draw["face"] == "" && $draw["suit"] == $card["suit"])
+      ||
+      ($draw["face"] == $card["face"] && $draw["suit"] == "")
+    ){
+      $match++;
+    };
+  }
+  return "found $match in $remaining";
+}
+
+
+//echo calculation_match($draw, $deck);
+
+echo implode(" of ", $draw) . ' : ' . calculate_match($draw, $deck);
 
 ?>
 
